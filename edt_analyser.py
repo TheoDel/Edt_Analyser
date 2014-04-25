9f9aabf58467220193698d218ace6c838f6c5e00
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-04-25 20:18:44 +0200

    Add a class Option to filter the slot we want to look at

--- edt_analyser.py
+++ edt_analyser.py
@@ -201,6 +201,15 @@ defaultSlots = [
 ]
 
 
+class Option: #Filter slots we want
+	
+	def __init__(self, listWeek, listDay, listSlot):
+		self.listWeek = listWeek
+		self.listDay = listDay
+		self.listSlot = listSlot
+
+	def isIn(self, result): #result : (week, day, slot)
+		return result[0] in self.listWeek and result[1] in self.listDay and result[2] in self.listSlot
 
 
 
