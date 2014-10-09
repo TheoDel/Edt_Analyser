208fac7578c535c9e7f18f7c0fee6a253c2269db
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-10-09 14:23:58 +0200

    Comments

--- GestionDatetime.py
+++ GestionDatetime.py
@@ -4,12 +4,15 @@ from datetime import time
 import locale # support of local locale (oh really ?), and internationalization
 import pytz
 
+""" Permet l'extraction des dates au format voulu, et dans la timezone de Paris """
 class GestionDatetime:
 	
+	""" Initialise la timezone et le format """
 	def __init__(self):
 		self.paris = pytz.timezone('Europe/Paris')
 		self.format = "%Y%m%dT%H%M%SZ"	
 	
+	""" Formate proprement la date passé en paramètre """
 	def getDatetime(self, my_date):
 		date = my_date.to_ical()
 		dtutcdate = pytz.utc.localize(datetime.strptime(date.decode(), self.format))
